85b4de78be5783a240e36c7e4b0f3404f8c1458f
oliverwest9 <oliverwest9@googlemail.com>
2024-06-30 14:36:19 +0100

    Set up HCP

--- main.tf
+++ main.tf
@@ -5,7 +5,13 @@ terraform {
       version = "~> 4.16"
     }
   }
+  cloud {
+    organization = "Olivers_Sandbox_Org"
 
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }  
   required_version = ">= 1.2.0"
 }
 
