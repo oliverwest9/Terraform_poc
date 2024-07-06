0b8f000b1f41f1652955c14573c4e8e21999d45d
oliverwest9 <oliverwest9@googlemail.com>
2024-07-06 17:00:36 +0100

    s3_use_path_style

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region  = var.region
+  skip_region_validation = true
+  s3_use_path_style = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
