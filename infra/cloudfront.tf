760dcbd4783c034e00cdf927e82c1c262978f2f4
oliverwest9 <oliverwest9@googlemail.com>
2024-07-07 14:06:55 +0100

    cloudfront infra added.

---
+++ infra/cloudfront.tf
@@ -0,0 +1,53 @@
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin access identity for static website"
+}
+
+resource "aws_cloudfront_distribution" "static_website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.static_website.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.front_end_bucket.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Static Website Distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.front_end_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "StaticWebsiteDistribution"
+  }
+}
